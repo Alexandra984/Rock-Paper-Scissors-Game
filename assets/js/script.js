096c7560b79bc2bf242aa200db7f2795b3d35c1f
Alexandra984 <alexandragrecu984@yahoo.com>
2019-12-29 01:12:15 +0200

    Implement the compareHands function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,60 @@ const game = ()=> {
             intro2.classList.add("fadeOut");
             gameArea.classList.add("fadeIn");
         });
-    }
-
-
-
+    };
     
-    
+    // Play Game
+    const playGame = () => {
+        const options = document.querySelectorAll(".options button");
+        console.log(options);
+        const computerOptions = ["rock", "paper", "scissors"];
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                // COMPARE HANDS
+                
+            });
+        });
+        const compareHands = (playerChoice, computerChoice) => {
+            const winner = document.querySelector(".winner-message");
+            
+            if (playerChoice === computerChoice) {
+                winner.textContent = "It is a tie";
+                return;
+            }
 
+            if (playerChoice === 'rock') {
+                if (computerChoice === 'scissors') {
+                    winner.textContent = "Player Wins";
+                    return;
+                } else {
+                    winner.textContent = "Computer Wins";
+                    return;
+                }
+            }
 
+            if (playerChoice === 'paper') {
+                if (computerChoice === 'scissors') {
+                    winner.textContent = "Computer Wins";
+                    return;
+                } else {
+                    winner.textContent = "Player Wins";
+                    return;
+                }
+            }
+
+            if (playerChoice === 'scissors') {
+                if (computerChoice === 'rock') {
+                    winner.textContent = "Computer Wins";
+                    return;
+                } else {
+                    winner.textContent = "Player Wins";
+                    return;
+                }
+            }
+        }
+    };
 
     // call functions
     startGame();
